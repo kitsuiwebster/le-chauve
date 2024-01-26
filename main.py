ce61086632958a7edaba60a5ad092f2725a76f80
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-26 20:53:34 +0100

    feat: check if the channel are empty

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from pydub import AudioSegment
 from song_titles import song_titles
 from discord.ext import tasks
+import asyncio
 
 load_dotenv()
 
@@ -92,11 +93,11 @@ song_titles = song_titles
 async def on_ready():
     print(f'{bot.user.name} is damn connected !!')
 
-    change_bot_identity.start()
+    # change_bot_identity.start()
 
     global voice_client
 
-    target_voice_channel_id = 1018098145420390410 
+    target_voice_channel_id = 1018098145420390410  # 1018098145420390410 
     target_voice_channel = bot.get_channel(target_voice_channel_id)
 
     bot.loop.create_task(change_status())
@@ -108,41 +109,39 @@ async def on_ready():
 
 
 
-bot_names = ["LE STRING", "LE SOFTEUR", "LA BÊTE", "L'EGIRL", "LE PAGO", "LA MOUCHE", "LE SUPPOSITOIRE", "LE COUPE-JARRET","LE NABOT", "LE PIED-BOUCHE", 
-             "LE FRÈRE", "LA BULLE", "LE NABUCHODONOSOR"]
-profile_pictures = ["./pics/00string.png", "./pics/09softeur.jpeg", "./pics/05bete.png", "./pics/01egirl.jpeg", "./pics/02pago.jpeg",
-                    "/03mouche.jpeg", "./pics/04suppositoire.jpeg", 
-                    "./pics/06coupe-jarret.jpeg","./pics/07nabot.jpeg", "./pics/08pied-bouche.png",  "./pics/10frere.png",
-                    "./pics/bulle.png", "./pics/nabu.png"]
+# bot_names = ["LE SUPPOSITOIRE", "LE STRING", "LE SOFTEUR", "LA BÊTE", "L'EGIRL", "LE PAGO", "LA MOUCHE", "LE SUPPOSITOIRE", "LE COUPE-JARRET","LE NABOT", "LE PIED-BOUCHE", 
+#              "LE FRÈRE", "LA BULLE", "LE NABUCHODONOSOR"]
+# profile_pictures = ["./pics/04suppositoire.jpeg", "./pics/00string.png", "./pics/09softeur.jpeg", "./pics/05bete.png", "./pics/01egirl.jpeg",
+#                     "./pics/02pago.jpeg","/03mouche.jpeg", "./pics/06coupe-jarret.jpeg","./pics/07nabot.jpeg", "./pics/08pied-bouche.png",
+#                     "./pics/10frere.png", "./pics/bulle.png", "./pics/nabu.png"]
 
-current_index = 0
+# current_index = 0
 
-@tasks.loop(hours=12)
-async def change_bot_identity():
-    global current_index 
+# @tasks.loop(hours=5)
+# async def change_bot_identity():
+#     global current_index 
 
-    print("Starting the process to change bot's identity")
+#     print("Starting the process to change bot's identity")
 
-    new_name = bot_names[current_index]
-    new_picture_path = profile_pictures[current_index]
+#     new_name = bot_names[current_index]
+#     new_picture_path = profile_pictures[current_index]
 
-    print(f"Selected new name: {new_name}")
-    print(f"Selected new profile picture: {new_picture_path}")
+#     print(f"Selected new name: {new_name}")
+#     print(f"Selected new profile picture: {new_picture_path}")
 
-    try:
-        await bot.user.edit(username=new_name)
-        print(f"Bot name changed to {new_name}")
+#     try:
+#         await bot.user.edit(username=new_name)
+#         print(f"Bot name changed to {new_name}")
 
-        with open(new_picture_path, 'rb') as img:
-            image_data = img.read()
-            await bot.user.edit(avatar=image_data)
-            print("Bot profile picture changed successfully")
+#         with open(new_picture_path, 'rb') as img:
+#             image_data = img.read()
+#             await bot.user.edit(avatar=image_data)
+#             print("Bot profile picture changed successfully")
 
-        current_index = (current_index + 1) % len(bot_names)
+#         current_index = (current_index + 1) % len(bot_names)
 
-    except Exception as e:
-        print(f"Error changing bot identity: {e}")
-
+#     except Exception as e:
+#         print(f"Error changing bot identity: {e}")
 
 
 
@@ -150,58 +149,74 @@ async def change_bot_identity():
 async def play_random_song():
     global voice_client
 
-    channel_ids = [534010128773414926, 811683007290146858, 1018098145420390410,
+    channel_ids = [534010128773414926, 811683007290146858, 1018098145420390410, 880881683749015622,
                    530012749066010657, 534010172587114508, 1017001428524486686]
 
     text_channel_id = 1199479426497380423
     text_channel = bot.get_channel(text_channel_id)
 
+    empty_channel_count = 0
+
     while True:
         try:
-            if not playlist:
-                target_voice_channel_id = random.choice(channel_ids)
-                target_voice_channel = bot.get_channel(target_voice_channel_id)
-
+            if empty_channel_count >= 15:
+                print("15 empty channels encountered. Disconnecting and waiting for 30 minutes.")
                 if voice_client:
                     await voice_client.disconnect()
-                voice_client = await target_voice_channel.connect()
+                await asyncio.sleep(1800)  
+                empty_channel_count = 0
+                continue
 
-                audio_files = [file for file in os.listdir("songs") if file.endswith((".mp3", ".wav"))]
+            target_voice_channel_id = random.choice(channel_ids)
+            target_voice_channel = bot.get_channel(target_voice_channel_id)
 
-                if not audio_files:
-                    print("No more songs in the 'songs' directory.")
-                    break
+            if voice_client:
+                await voice_client.disconnect()
+            voice_client = await target_voice_channel.connect()
 
-                random.shuffle(audio_files)
-                random_audio_file = audio_files.pop(0)
-                audio_file_path = os.path.join("songs", random_audio_file)
+            if len([member for member in target_voice_channel.members if member != bot.user]) == 0:
+                print(f"Voice channel {target_voice_channel.name} is empty. Moving to the next channel.")
+                empty_channel_count += 1
+                continue
+            else:
+                empty_channel_count = 0
 
-                audio = AudioSegment.from_mp3(audio_file_path)
-                audio_duration = len(audio) / 1000
+            audio_files = [file for file in os.listdir("songs") if file.endswith((".mp3", ".wav"))]
 
-                audio_source = FFmpegPCMAudio(executable="ffmpeg", source=audio_file_path)
-                try:
-                    if not voice_client.is_playing():
-                        voice_client.play(audio_source)
-                        song_title = song_titles.get(random_audio_file, 'Unknown')
-                        await text_channel.send(f'Vous écoutez désormais: {song_title}')
-                    else:
-                        print("Bot is already playing.")
-                except (ConnectionClosed, ClientException) as e:
-                    print(f"Error occurred: {e}")
-                    break
+            if not audio_files:
+                print("No more songs in the 'songs' directory.")
+                break
 
-                await asyncio.sleep(audio_duration)
+            random.shuffle(audio_files)
+            random_audio_file = audio_files.pop(0)
+            audio_file_path = os.path.join("songs", random_audio_file)
 
-                wait_time_seconds = random.randint(120, 240)
-                print(f"""
-                      -------------------------------------------------------
-                      -------------------------------------------------------
-                      Waiting for {wait_time_seconds // 60 } minutes before playing the next romeo meme.
-                      -------------------------------------------------------
-                      -------------------------------------------------------
-                      """)
-                await asyncio.sleep(wait_time_seconds)
+            audio = AudioSegment.from_mp3(audio_file_path)
+            audio_duration = len(audio) / 1000
+
+            audio_source = FFmpegPCMAudio(executable="ffmpeg", source=audio_file_path)
+            try:
+                if not voice_client.is_playing():
+                    voice_client.play(audio_source)
+                    song_title = song_titles.get(random_audio_file, 'Unknown')
+                    await text_channel.send(f'Vous écoutez désormais: {song_title}')
+                else:
+                    print("Bot is already playing.")
+            except (ConnectionClosed, ClientException) as e:
+                print(f"Error occurred: {e}")
+                break
+
+            await asyncio.sleep(audio_duration)
+
+            wait_time_seconds = random.randint(120, 240)
+            print(f"""
+                  -------------------------------------------------------
+                  -------------------------------------------------------
+                  Waiting for {wait_time_seconds // 60} minutes before playing the next song.
+                  -------------------------------------------------------
+                  -------------------------------------------------------
+                  """)
+            await asyncio.sleep(wait_time_seconds)
 
         except discord.errors.ConnectionClosed as e:
             print(f"Disconnected from voice with error: {e}")
@@ -210,6 +225,8 @@ async def play_random_song():
             target_voice_channel = bot.get_channel(target_voice_channel_id)
             voice_client = await target_voice_channel.connect()
             continue
+
+
 
 
 
