42062a1ef311fd8b8895d2b5d7254636f63271ac
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-27 18:32:04 +0100

    fix: identity order

--- main.py
+++ main.py
@@ -113,11 +113,11 @@ async def on_ready():
 
 
 
-bot_names = ["LE SOFTEUR", "LE SUPPOSITOIRE", "LE STRING", "LA BÊTE", "L'EGIRL", "LE PAGO", "LA MOUCHE", "LE SUPPOSITOIRE", "LE COUPE-JARRET","LE NABOT", "LE PIED-BOUCHE", 
-             "LE FRÈRE", "LA BULLE", "LE NABUCHODONOSOR", "LA NOUILLE", "LE MALOTRU", "LA FLAQUE"]
-profile_pictures = ["./pics/09softeur.jpeg", "./pics/04suppositoire.jpeg", "./pics/00string.png", "./pics/05bete.png", "./pics/01egirl.jpeg",
-                    "./pics/02pago.jpeg","/03mouche.jpeg", "./pics/06coupe-jarret.jpeg","./pics/07nabot.jpeg", "./pics/08pied-bouche.png",
-                    "./pics/10frere.png", "./pics/bulle.png", "./pics/nabu.png", "./pics/nouille.png", "./pics/malotru.png", "./pics/flaque"]
+bot_names = ["LA MOUCHE", "LE SOFTEUR", "LE SUPPOSITOIRE", "LE STRING", "LA BÊTE", "L'EGIRL", "LE PAGO", "LA MOUCHE", "LE SUPPOSITOIRE", "LE COUPE-JARRET","LE NABOT", "LE PIED-BOUCHE", 
+             "LA BULLE", "LE FRÈRE", "LE NABUCHODONOSOR", "LA NOUILLE", "LE MALOTRU", "LA FLAQUE"]
+profile_pictures = ["/03mouche.jpeg", "./pics/09softeur.jpeg", "./pics/04suppositoire.jpeg", "./pics/00string.png", "./pics/05bete.png", "./pics/01egirl.jpeg",
+                    "./pics/02pago.jpeg","./pics/06coupe-jarret.jpeg","./pics/07nabot.jpeg", "./pics/08pied-bouche.png", "./pics/bulle.png", 
+                    "./pics/10frere.png", "./pics/nabu.png", "./pics/nouille.png", "./pics/malotru.png", "./pics/flaque"]
 
 current_index = 0
 
