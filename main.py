301e6adc955aa5b8894a406536950ea1e5be304d
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-28 22:38:06 +0100

    fix: identity order

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ async def on_ready():
 
 bot_names = ["LA BÊTE", "LA MOUCHE", "LE COUPE-JARRET", "LE SOFTEUR", "LE SUPPOSITOIRE", "LE STRING", "L'EGIRL", "LE PAGO", "LE NABOT", "LE PIED-BOUCHE", 
              "LA BULLE", "LE FRÈRE", "LE NABUCHODONOSOR", "LA NOUILLE", "LE MALOTRU", "LA FLAQUE"]
-profile_pictures = ["./pics/05bete.png", "/pics/03mouche.jpeg", "./pics/06coupe-jarret.jpeg", "./pics/09softeur.jpeg", "./pics/04suppositoire.jpeg", "./pics/00string.png", "./pics/01egirl.jpeg",
+profile_pictures = ["/pics/03mouche.jpeg", "./pics/05bete.png", "./pics/06coupe-jarret.jpeg", "./pics/09softeur.jpeg", "./pics/04suppositoire.jpeg", "./pics/00string.png", "./pics/01egirl.jpeg",
                     "./pics/02pago.jpeg", "./pics/07nabot.jpeg", "./pics/08pied-bouche.png", "./pics/bulle.png", 
                     "./pics/10frere.png", "./pics/nabu.png", "./pics/nouille.png", "./pics/malotru.png", "./pics/flaque"]
 
