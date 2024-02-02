3fa4b01caf7fa0ec501134399e8ae7f5f268a8ab
kitsuiwebster <raphmartin1999@gmail.com>
2024-02-02 16:19:21 +0100

    fix: identity order

--- main.py
+++ main.py
@@ -113,10 +113,11 @@ async def on_ready():
 
 
 
-bot_names = ["LA FLAQUE", "LA BULLE", "LA NOUILLE",  "LA MOUCHE", "LE PIED-BOUCHE", "LE COUPE-JARRET", "LE SOFTEUR", "LE SUPPOSITOIRE", "LE STRING", "L'EGIRL", "LE PAGO",
+bot_names = ["LE SUPPOSITOIRE", "LE COUPE-JARRET", "LE SOFTEUR", "LA FLAQUE", "LA BULLE", "LA NOUILLE",  "LA MOUCHE", "LE PIED-BOUCHE", "LE STRING", "L'EGIRL", "LE PAGO",
              "LE NABOT", "LE FRÈRE", "LE NABUCHODONOSOR", "LE MALOTRU", "LA BÊTE"]
-profile_pictures = ["./pics/flaque.png", "./pics/bulle.png", "./pics/nouille.png", "./pics/03mouche.jpeg", "./pics/08pied-bouche.png", "./pics/06coupe-jarret.jpeg", "./pics/09softeur.jpeg",
-                    "./pics/04suppositoire.jpeg", "./pics/00string.png", "./pics/01egirl.jpeg",
+profile_pictures = ["./pics/04suppositoire.jpeg", "./pics/06coupe-jarret.jpeg", "./pics/09softeur.jpeg", "./pics/flaque.png",
+                    "./pics/bulle.png", "./pics/nouille.png", "./pics/03mouche.jpeg", "./pics/08pied-bouche.png",
+                    "./pics/00string.png", "./pics/01egirl.jpeg",
                     "./pics/02pago.jpeg", "./pics/07nabot.jpeg", 
                     "./pics/10frere.png", "./pics/nabu.png", "./pics/malotru.png", "./pics/05bete.png"]
 
