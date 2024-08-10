024bd55ae0bcbca81cf4ac990a9071b7581da14c
kitsuiwebster <raphmartin1999@gmail.com>
2024-08-10 12:21:38 +0200

    feat: private messages

--- main.py
+++ main.py
@@ -21,14 +21,11 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-
-
 
 class NextCog(commands.Cog):
     def __init__(self, bot, voice_client):
@@ -49,26 +46,20 @@ class NextCog(commands.Cog):
             return
 
         try:
-
             voice_client.stop()
             print("Next song will play")
             await play_random_song()
             
         except Exception as e:
-            print(f"Error occurred tg: {e}")
+            print(f"Error occurred: {e}")
 
 async def setup(bot, voice_client):
     cog = NextCog(bot, voice_client)
     bot.add_cog(cog)
     return cog
 
-
-
-
-
 async def change_status():
     await bot.wait_until_ready()
-
     while not bot.is_closed():
         statuses = [
             discord.Game(name="TG!"),
@@ -76,7 +67,6 @@ async def change_status():
             discord.Game(name="MA BEUTEU???"),
             discord.Game(name="NON!"),
         ]
-
         for status in statuses:
             await bot.change_presence(activity=status)
             await asyncio.sleep(10)
@@ -96,31 +86,27 @@ async def on_ready():
 {bot.user.name} is damn connected !!
 =============================================================          
 """)
-
     change_bot_identity.start()
-
+    send_private_message.start()
     global voice_client
-
-    target_voice_channel_id = 1018098145420390410  # 1018098145420390410 
+    target_voice_channel_id = 1018098145420390410  
     target_voice_channel = bot.get_channel(target_voice_channel_id)
-
     bot.loop.create_task(change_status())
     if target_voice_channel:
-        if voice_client is None:
+        if voice_client is None or not hasattr(voice_client, 'is_connected') or not voice_client.is_connected():
             voice_client = await target_voice_channel.connect()
         print("Bot is connected to the voice channel and starting to play music.")
         await play_random_song()
+    
 
-
-
-bot_names = ["LE CORBEAU", "LE SOFTEUR", "LE PAGO", "LA BÊTE", "LA MOUCHE", "LE SUPPOSITOIRE", "LE COUPE-JARRET", 
-             "LE NABUCHODONOSOR", "LA BULLE", "LA NOUILLE",  "LE PIED-BOUCHE",
+bot_names = ["LE SOFTEUR", "LE PAGO", "LA BÊTE", "LA MOUCHE", "LE SUPPOSITOIRE", "LE COUPE-JARRET", 
+             "LE NABUCHODONOSOR", "LA BULLE", "LA NOUILLE",  "LE PIED-BOUCHE", "LE CORBEAU", 
              "LE STRING", "L'EGIRL", "LE GOAT", "LE FRÈRE", 
              "LE NABOT", "LE MALOTRU", "L'ÉBOUEUR", "LA POUTRE", "LA FLAQUE"]
-profile_pictures = ["./pics/corbeau.png", "./pics/09softeur.jpeg", "./pics/02pago.png", "./pics/05bete.png", "./pics/03mouche.jpeg",
+profile_pictures = ["./pics/09softeur.jpeg", "./pics/02pago.png", "./pics/05bete.png", "./pics/03mouche.jpeg",
                     "./pics/04suppositoire.jpeg", "./pics/06coupe-jarret.jpeg",
                     "./pics/nabu.png", 
-                    "./pics/bulle.png", "./pics/nouille.png", "./pics/08pied-bouche.png",
+                    "./pics/bulle.png", "./pics/nouille.png", "./pics/08pied-bouche.png", "./pics/corbeau.png", 
                     "./pics/00string.png", "./pics/egirl.jpeg",
                     "./pics/goat.jpeg", "./pics/frere.png", "./pics/07nabot.jpeg", 
                     "./pics/malotru.png", "./pics/eboueur.png", "./pics/poutre.jpeg", "./pics/flaque.png"]
@@ -129,13 +115,10 @@ current_index = 0
 
 @tasks.loop(hours=6)
 async def change_bot_identity():
-    global current_index 
-
+    global current_index
     print("Starting the process to change bot's identity")
-
     new_name = bot_names[current_index]
     new_picture_path = profile_pictures[current_index]
-
     print(f"""
 =============================================================
 Selected new name: {new_name}
@@ -149,7 +132,6 @@ Selected new profile picture: {new_picture_path}
 Bot name changed to {new_name}
 =============================================================
 """)
-
         with open(new_picture_path, 'rb') as img:
             image_data = img.read()
             await bot.user.edit(avatar=image_data)
@@ -158,30 +140,20 @@ Bot name changed to {new_name}
 Bot profile picture changed successfully
 =============================================================
 """)
-
         current_index = (current_index + 1) % len(bot_names)
-
     except Exception as e:
         print(f"Error changing bot identity: {e}")
 
 
-
-
 async def play_random_song():
     global voice_client
-
     ignored_user_id = 1161683957440589976
-
     channel_ids = [534010128773414926, 811683007290146858, 1018098145420390410, 880881683749015622,
                    530012749066010657, 534010172587114508, 1017001428524486686, 1150149299028635731, 1216106779138850976]
-
     text_channel_id = 1199479426497380423
     text_channel = bot.get_channel(text_channel_id)
-
     wait_channel_id = 1200530884831477841
-
     empty_channel_count = 0
-
     while True:
         try:
             if empty_channel_count >= 20:
@@ -195,14 +167,11 @@ async def play_random_song():
                 await asyncio.sleep(1800)
                 empty_channel_count = 0
                 continue
-
             target_voice_channel_id = random.choice(channel_ids)
             target_voice_channel = bot.get_channel(target_voice_channel_id)
-
             if voice_client:
                 await voice_client.disconnect()
             voice_client = await target_voice_channel.connect()
-
             channel_members = [member for member in target_voice_channel.members if member.id != ignored_user_id and member != bot.user]
             if len(channel_members) == 0:
                 print(f"""
@@ -214,20 +183,16 @@ Voice channel {target_voice_channel.name} is empty. Moving to the next channel.
                 continue
             else:
                 empty_channel_count = 0
-
             print(f"""
 =============================================================
 Currently in voice channel: {target_voice_channel.name}
 Members in voice channel: {[member.name for member in channel_members]}
 =============================================================
 """)
-
             audio_files = [file for file in os.listdir("songs") if file.endswith((".mp3", ".wav"))]
-
             if not audio_files:
                 print("No more songs in the 'songs' directory.")
                 break
-
             random.shuffle(audio_files)
             random_audio_file = audio_files.pop(0)
             print(f"""
@@ -235,12 +200,9 @@ Members in voice channel: {[member.name for member in channel_members]}
 Playing song: {random_audio_file}
 =============================================================
 """)
-            
             audio_file_path = os.path.join("songs", random_audio_file)
-
             audio = AudioSegment.from_mp3(audio_file_path)
             audio_duration = len(audio) / 1000
-
             audio_source = FFmpegPCMAudio(executable="ffmpeg", source=audio_file_path)
             try:
                 if not voice_client.is_playing():
@@ -252,16 +214,13 @@ Playing song: {random_audio_file}
             except (ConnectionClosed, ClientException) as e:
                 print(f"Error occurred: {e}")
                 break
-
             await asyncio.sleep(audio_duration)
-
             print("""
 =============================================================
 Waiting for 10 minutes in the channel after playing the song.
 =============================================================
 """)
             await asyncio.sleep(600)
-
             wait_channel = bot.get_channel(wait_channel_id)
             if wait_channel:
                 if voice_client:
@@ -273,7 +232,6 @@ Bot is in {wait_channel.name}. Waiting for 10 minutes.
 =============================================================
 """)
                 await asyncio.sleep(600)
-
         except discord.errors.ConnectionClosed as e:
             print(f"Disconnected from voice with error: {e}")
             print("Attempting to reconnect...")
@@ -284,7 +242,46 @@ Bot is in {wait_channel.name}. Waiting for 10 minutes.
 
 
 
+user_ids = [530012026114670593, 1163037440852893756, 447164227321331715, 502900113040212019, 694257248532299881, 435469813951889408, 
+            750289831719862334, 428264487754268672,
+            211868111844933633, 332522721269383169, 256128167041957888, 720003437861011578, 404065631386992660, 539142544098066446,
+            257458706738839553, 531874992141500416, 248855519035523085, 300368857346998287, 259364875770265600, ] 
+messages = ["https://tenor.com/view/geneva-kitsui-koni-kitsui-koni-mountain-gif-98006767313222820",
+            "👀", "Ouais de ouf",
+            "https://tenor.com/view/pago-kitsui-koni-kitsui-koni-vcg-gif-5542109184676738895",
+            "https://www.youtube.com/watch?v=r3edq2MiH3o&list=PLke_eeiQwDHNNPW2QwBKf8J4-rO59m6ha&index=8",
+            "https://drive.google.com/uc?id=1rmy6SojTegp5NkYMX_pohtwzZQm4zpHi&export=download",
+            "https://drive.google.com/uc?id=1K9XQK8fmIW1sKOwFWKzrln1QfgznnpxU&export=download",
+            "https://drive.google.com/uc?id=1CJZj45lzULSR4qXq4Ls3uJANPMHro9_Q&export=download",
+            ]
+message_index = 0
 
+@tasks.loop(seconds=40000) 
+async def send_private_message():
+    global message_index
+    user_id = user_ids[message_index % len(user_ids)]
+    user = await bot.fetch_user(user_id)
+    if user:
+        try:
+            await user.send(messages[message_index])
+            print(f"""
+=============================================================
+Sent '{messages[message_index]}' to {user_id}
+=============================================================
+""")
+        except Exception as e:
+            print(f"""
+=============================================================
+Failed to send message to {user_id}: {e}
+=============================================================
+""")
+    message_index = 1 - message_index
+
+@send_private_message.before_loop
+async def before_send_private_message():
+    print("Waiting for bot to be ready...")
+    await bot.wait_until_ready()
+    print("Starting to send private messages.")
 
 
 
@@ -293,12 +290,9 @@ bot_token = os.getenv("DISCORD_BOT_TOKEN")
 async def run_bot():
     try:
         print("oui")
-
+        
     except Exception as e:
         print(f"Error loading extension: {e}")
-
-
-
 
 if __name__ == "__main__":
     try:
